47225b3d391b8ddfdc08660de1621a44d0435478
baranero <jakub.baran2@gmail.com>
2022-12-27 15:33:47 +0100

    added meals items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <React.Fragment>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from './Header.module.css'
+import backgroundImage from '../../img/meals.jpg'
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton/>
+      </header>
+      
+      <div className={classes['main-image']}>
+        <img src={backgroundImage} alt="A table with food"/>
+      </div>
+    </React.Fragment>
+
+  )
+}
+
+export default Header
