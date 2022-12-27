c1459224ba3eb78002d56b08c52805d35bf0a4b0
baranero <jakub.baran2@gmail.com>
2022-12-27 17:16:33 +0100

    app basics for further develop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false)
+
   return (
     <React.Fragment>
+      <Cart />
       <Header/>
       <main>
         <Meals/>
