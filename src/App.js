9a797f3bc4082181726459043cd2dd4d9bbad1cd
baranero <jakub.baran2@gmail.com>
2022-12-27 19:19:34 +0100

    add useContext and Provider

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
 
   const [cartIsShown, setCartIsShown] = useState(false)
 
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
+
   return (
-    <React.Fragment>
-      <Cart />
-      <Header/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
